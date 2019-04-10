e0ebacb8ade1d78393e37ad8cdbe802444c66770
HaQadosch Berraka <haqadosch@pm.me>
2019-04-10 11:17:05 +0100

    Updates

---
+++ precache-manifest.c53504d82c8c6cee12f1699f5c3bf471.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06ebbd1e2404b14947a0",
+    "url": "/Calculator/static/css/main.3daae395.chunk.css"
+  },
+  {
+    "revision": "06ebbd1e2404b14947a0",
+    "url": "/Calculator/static/js/main.2bbc0ccc.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/Calculator/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "32c07487921b266312e5a96080c160e8",
+    "url": "/Calculator/index.html"
+  }
+];
